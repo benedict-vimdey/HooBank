15381d8defdb236a6a5699543552afaba17a7c17
benedict-vimdey <benedictvimdey@gmail.com>
2023-02-19 10:22:41 +0000

    added the hero section

--- HooBank/src/components/GetStarted.jsx
+++ HooBank/src/components/GetStarted.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { arrowUp } from '../assets'
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className='flex items-center justify-center w-[140px] h-[140px] rounded-full bg-blue-400 p-[2px] cursor-pointer '>
+      <div className='flex items-center justify-center flex-col bg-primary h-[100%] w-[100%] rounded-full'>
+        <div className='flex justify-center items-start'>
+          <p className='font-poppins font-medium text-[18px] leading-[23px] '>
+            <span className='text-blue-400'>Get </span>
+          </p>
+            <img src={arrowUp} alt='arrow up' className='w-[23px] h-[23px] object-contain ml-2 ' />
+        </div>
+
+        <div className='flex justify-center items-start'>
+          <p className='font-poppins font-medium text-[18px] leading-[23px] '>
+            <span className='text-blue-400'>Started</span>
+          </p>            
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- HooBank/src/components/Hero.jsx
+++ HooBank/src/components/Hero.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { discount, robot } from '../assets'
+import GetStarted from './GetStarted'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
+const Hero = () =>  (
+    <section id='home' className='flex md:flex-row flex-col sm:py-16 py-6'>
+      <div className='flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-16 px-6'>
+        <div className='flex items-center  bg-gradient-to-r from-[#272727] to-[#11101D] rounded-[10px] px-4 py-[6px] mb-2'>
+          <img src={discount} alt='discount' className='w-[32px] h-[32px] ' />
+          <p className='font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] ml-2'>
+            <span className='text-white'>20%</span> DISCOUNT FOR {" "}
+            <span className='text-white'>1 MONTH</span> ACCOUNT
+          </p>
+        </div>
+
+        <div className='flex justify-between items-center w-full'>
+          <h1 className='flex-1 font-poppins font-semibold text-white text-[52px] leading-[75px] '>
+            The Next <br className='sm:block hidden'/> {" "}
+            <span className='text-blue-400'>Generation</span> 
+          </h1>
+
+          <div className='ss:flex-1 md:mr-4 mr-0 '>
+            <GetStarted/>
+          </div>
+        </div>
+
+        <h1 className='font-poppins font-semibold text-white text-[52px] leading-[75px] ss:leading-[100px] '>
+          Payment Method
+        </h1>
+
+        <h1 className='font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5 '>
+          Our team of experts uses a methodology to identify <br/> the credit cards most likely to fit your needs. <br/>
+          We examine annual percentage rates, annual fees.
+        </h1>
+
+      </div>
+
+      <div className='flex-1 flex justify-center items-center md:my-0 my-10 relative'>
+        <img src={robot} alt='robot' className='w-[100%] h-[100%] relative z-[5]' />
+      </div>
+
+    </section>
   )
-}
+
 
 export default Hero
