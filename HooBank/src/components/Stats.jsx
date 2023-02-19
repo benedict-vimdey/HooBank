df4cbfcec756a477d95f070633110db47708fd75
benedict-vimdey <benedictvimdey@gmail.com>
2023-02-19 11:00:59 +0000

    updated the hero section and added stats section too

--- HooBank/src/components/Stats.jsx
+++ HooBank/src/components/Stats.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
+const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
+
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <div className='flex justify-center items-center flex-wrap sm:mb-20 mb-6'>
+      {stats.map((stat) => (
+        <div key={stat.id} className='text-white flex items-center p-16 m-3 flex-1 '>
+          <p className='font-poppins font-semibold text-[40px] xs:font-[40px] xs:leading-[53px] leading-[43px] '>{stat.value}</p>
+          <p className='font-poppins font-normal text-[20px] text-blue-400 ml-3 '>{stat.title}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
